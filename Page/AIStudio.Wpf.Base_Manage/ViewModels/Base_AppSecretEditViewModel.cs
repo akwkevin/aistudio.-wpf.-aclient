35c2d892c20be059de37e7a4a126795a16b4d4d7
艾竹 <akwkevin@126.com>
2022-10-11 22:13:28 +0800

    小bug

--- Page/AIStudio.Wpf.Base_Manage/ViewModels/Base_AppSecretEditViewModel.cs
+++ Page/AIStudio.Wpf.Base_Manage/ViewModels/Base_AppSecretEditViewModel.cs
@@ -7,14 +7,7 @@ namespace AIStudio.Wpf.Base_Manage.ViewModels
     {
         public Base_AppSecretEditViewModel(Base_AppSecretDTO data, string area, string identifier, string title = "编辑表单") : base(data, area, identifier, title)
         {
-            if (Data == null)
-            {
-                Data = new Base_AppSecretDTO();
-            }
-            else
-            {
-                GetData(Data);
-            }
+
         }
 
         protected override void GetData(Base_AppSecretDTO para)
